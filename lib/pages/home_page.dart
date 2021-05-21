b599f6ff44f3a38c84155af9333be5a85dbafb42
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2021-05-21 11:26:53 -0300

    Main Page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,69 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          // Add the app bar to the CustomScrollView.
+          SliverAppBar(
+            pinned: true,
+            floating: false,
+            snap: false,
+            leading: Padding(
+                padding: EdgeInsets.only(left: 20), child: Icon(Icons.ac_unit)),
+            actions: [
+              Icon(Icons.search),
+              Padding(
+                  padding: EdgeInsets.only(left: 5, right: 20),
+                  child: Icon(Icons.more_vert))
+            ],
+            expandedHeight: 200.0,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text('All Events',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  )),
+              background: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        "https://images.pexels.com/photos/443383/pexels-photo-443383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: new Container(
+                  decoration: new BoxDecoration(
+                      color: Color(0xFF1C1C1E).withOpacity(0.6)),
+                ),
+              ),
+            ),
+          ),
+          // Next, create a SliverList
+          SliverList(
+            // Use a delegate to build items as they're scrolled on screen.
+            delegate: SliverChildBuilderDelegate(
+              // The builder function returns a ListTile with a title that
+              // displays the index of the current item.
+              (context, index) => ListTile(title: Text('Item #$index')),
+              // Builds 1000 ListTiles
+              childCount: 1000,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
